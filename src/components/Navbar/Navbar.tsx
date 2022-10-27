19d312f335504fd32ffe335eddd838a51b319be6
Jennifer Dobyns <jennifer.dobyns@hotmail.com>
2022-10-27 14:57:37 -0600

    Initial commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+import Nav from "react-boostrap";
+
+class NavBar extends Component{
+    render(){
+        return{
+
+        }
+    }
+}
+
+export default NavBar
